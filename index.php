456f3fe9fc605bfa4c57da23b88591ae9ef878f7
BULLSKAR <91764876+BULLSKAR@users.noreply.github.com>
2021-10-02 02:43:35 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Antonio López Benedicto</title>
+    <!-- FAVICON -->
+    <link  rel="icon"   href="images/logo.png" type="image/png" />
+    <!-- FONT AWESOME -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css">
+    <!-- FONT OSWALD -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400&display=swap" rel="stylesheet">
+    <!-- CUSTOM CSS -->
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="container">
+        <nav class="nav-main">
+            <a href="index.php" ><img src="images/banner.png" alt="cat antonio" class="nav-brand"></a>
+            <ul class="nav-menu">
+                <li>
+                    <a href="index.php"><i class="fas fa-home"></i> Inicio</a>
+                </li>
+                <li>
+                    <a href="servicios.html"><i class="far fa-sticky-note"></i> Servicios</a>
+                </li>
+                <li>
+                    <a href="contacto.html"><i class="far fa-id-card"></i> Contacto</a>
+                </li>
+            </ul>
+        </nav>
+        
+        
+    </div>
+    
+</body>
+</html>
