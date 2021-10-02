af1388a7321fe8992d745ea86f4fa6035b875302
BULLSKAR <91764876+BULLSKAR@users.noreply.github.com>
2021-10-02 02:26:54 +0200

    Add files via upload

--- submeter-formulario.php
+++ submeter-formulario.php
@@ -1,17 +1,17 @@
 <?php
-$name = $_POST['nombre'];
-$mail = $_POST['email'];
-$phone = $_POST['telefono'];
-$message = $_POST['assunto'];
+$name = $_POST['name'];
+$mail = $_POST['mail'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
 
 $header = 'From: ' . $mail . " \r\n";
 $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
 $header .= "Mime-Version: 1.0 \r\n";
 $header .= "Content-Type: text/plain";
 
-$message = "Este mensaje fue enviado por: " . $nombre . " \r\n";
-$message .= "Su e-mail es: " . $email . " \r\n";
-$message .= "Teléfono de contacto: " . $telefono . " \r\n";
+$message = "Este mensaje fue enviado por: " . $name . " \r\n";
+$message .= "Su e-mail es: " . $mail . " \r\n";
+$message .= "Teléfono de contacto: " . $phone . " \r\n";
 $message .= "Mensaje: " . $_POST['message'] . " \r\n";
 $message .= "Enviado el: " . date('d/m/Y', time());
 
